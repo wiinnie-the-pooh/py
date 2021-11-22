1a9ec72d2f1580353d75989e4049ff25633c6de5
Alexey Petrov <alexey.petrov@opencascade.com>
2021-11-22 20:57:09 +0300

    31. Next Permutation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import collections
 import re
 from typing import *
 from collections import *
@@ -31,34 +32,6 @@ def nodes2list(node):
     return res
 
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-
-def treeNodeToString(root):
-    if not root:
-        return "[]"
-    output = ""
-    queue = [root]
-    current = 0
-    while current != len(queue):
-        node = queue[current]
-        current = current + 1
-
-        if not node:
-            output += "null, "
-            continue
-
-        output += str(node.val) + ", "
-        queue.append(node.left)
-        queue.append(node.right)
-    return "[" + output[:-2] + "]"
-
-
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -85,7 +58,7 @@ def tree2list(root):
         lastval = current
         queue.append(node.left)
         queue.append(node.right)
-        
+
     return output[:lastval]
 
 
@@ -100,7 +73,7 @@ def list2tree(input):
 
         item = input[index]
         index = index + 1
-        if item != "null":
+        if item != None:
             leftNumber = item
             node.left = TreeNode(leftNumber)
             nodeQueue.append(node.left)
@@ -110,7 +83,7 @@ def list2tree(input):
 
         item = input[index]
         index = index + 1
-        if item != "null":
+        if item != None:
             rightNumber = item
             node.right = TreeNode(rightNumber)
             nodeQueue.append(node.right)
@@ -121,19 +94,26 @@ def list2tree(input):
 t = [6, 2, 8, 0, 4, 7, 9, None, None, 3, 5]
 assert tree2list(list2tree(t)) == t
 
-
+from collections import *
 class Solution:
-    def run(self, triangle: List[List[int]]) -> int:
-        endi = len(triangle)
-        acc = [[0 for _ in range(i + 1)] for i in range(endi + 1)]
-        for i in range(endi - 1, -1, -1):
-            ii = i + 1
-            for j in range(i + 1):
-                acc[i][j] = min(acc[ii][j], acc[ii][j + 1]) + triangle[i][j]
-                pass
+    def run(self, nums: List[int]) -> None:
+        endi = len(nums)
+        last = endi - 1
+        for i in range(last, 0, -1):
+            if nums[i - 1] < nums[i]:
+                for j in range(last, i - 1, -1):
+                    if nums[j] > nums[i - 1]:
+                        nums[i - 1], nums[j] = nums[j], nums[i - 1]
+                        l = i; r = last
+                        while l < r:
+                            nums[l], nums[r] = nums[r], nums[l]
+                            l += 1
+                            r -= 1
+                        return nums
+        nums.sort()
+        return nums
 
-        return acc[0][0]
-
-
-assert Solution().run(triangle=[[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]) == 11
-assert Solution().run(triangle=[[-10]]) == -10
+assert Solution().run(nums = [1,3,2]) == [2,1,3]
+assert Solution().run(nums = [3,2,1]) == [1,2,3]
+assert Solution().run(nums = [4,5,2,6,7,3,1]) == [4,5,2,7,1,3,6]
+assert Solution().run(nums = [1,2,3]) == [1,3,2]
