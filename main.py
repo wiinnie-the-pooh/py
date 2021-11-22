265ca7b58d32f951607742cef9b76e3881aa90e9
Alexey Petrov <alexey.petrov@opencascade.com>
2021-11-22 12:08:16 +0300

    PlayGround / TreeNode

--- main.py
+++ main.py
@@ -31,21 +31,109 @@ def nodes2list(node):
     return res
 
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+def treeNodeToString(root):
+    if not root:
+        return "[]"
+    output = ""
+    queue = [root]
+    current = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
+
+        if not node:
+            output += "null, "
+            continue
+
+        output += str(node.val) + ", "
+        queue.append(node.left)
+        queue.append(node.right)
+    return "[" + output[:-2] + "]"
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def tree2list(root):
+    if not root:
+        return []
+    output = []
+    queue = [root]
+    current = 0
+    lastval = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
+
+        if not node:
+            output.append(node)
+            continue
+
+        output.append(node.val)
+        lastval = current
+        queue.append(node.left)
+        queue.append(node.right)
+        
+    return output[:lastval]
+
+
+def list2tree(input):
+    root = TreeNode(input[0])
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(input):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = input[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = item
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(input):
+            break
+
+        item = input[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = item
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+
+    return root
+
+
+t = [6, 2, 8, 0, 4, 7, 9, None, None, 3, 5]
+assert tree2list(list2tree(t)) == t
+
+
 class Solution:
     def run(self, triangle: List[List[int]]) -> int:
         endi = len(triangle)
-        def step(i: int, j: int) -> int:
+        acc = [[0 for _ in range(i + 1)] for i in range(endi + 1)]
+        for i in range(endi - 1, -1, -1):
             ii = i + 1
-            acc = triangle[i][j]
-            if ii == endi:
-                return acc
-            acc1 = step(ii, j)
-            acc2 = step(ii, j + 1)
-            acc += min(acc1, acc2)
-            return acc
-        
-        res = step(0, 0)
-        return res
+            for j in range(i + 1):
+                acc[i][j] = min(acc[ii][j], acc[ii][j + 1]) + triangle[i][j]
+                pass
 
-assert Solution().run(triangle = [[-10]]) == -10
-assert Solution().run(triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]) == 11
+        return acc[0][0]
+
+
+assert Solution().run(triangle=[[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]) == 11
+assert Solution().run(triangle=[[-10]]) == -10
